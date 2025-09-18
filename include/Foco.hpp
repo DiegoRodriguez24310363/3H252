b6e01e363a74439a317ee9bbdae3026f75e1795d
Cepillin <a24310363@live.ceti.mx>
2025-09-18 01:55:16 +0000

    Practica1

---
+++ include/Foco.hpp
@@ -0,0 +1,15 @@
+#pragma onece
+#include <EstadoFoco.hpp>
+
+class Foco
+{
+private:
+    EstadoFoco estadoActual;
+public:
+    Foco() { estadoActual=false}
+    ~Foco() {}
+    void Encender(){ }
+    void Apagar(){}
+    EstadoFoco MostrarEstado(){}
+};
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <Foco.hpp>
 using namespace std;
 int main(int argc, char const *argv[])
 {
+    Foco f;
     cout<<"hola mundo"<<endl;
     return 0;
 }
